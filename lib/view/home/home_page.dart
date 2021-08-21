665440c805c17fd60df245fdd2e776694f9fe70c
mslxl <i@mslxl.com>
2021-08-21 18:51:23 +0800

    fix: 搜索不到标签内容

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -7,6 +7,7 @@ import 'package:meme_man/view/add/add_page.dart';
 import 'package:meme_man/db/config_db.dart';
 import 'package:meme_man/view/config/config_page.dart';
 import 'package:meme_man/db/meme_db.dart';
+import 'package:meme_man/view/home/search_bar_delegate.dart';
 import 'package:waterfall_flow/waterfall_flow.dart';
 
 import 'home_controller.dart';
@@ -53,7 +54,7 @@ class HomePage extends StatelessWidget {
             onPressed: () {
               showSearch(
                   context: context,
-                  delegate: _SearchBarDelegate(_waterfallBuilder));
+                  delegate: SearchBarDelegate(_waterfallBuilder));
             },
           ),
           IconButton(
@@ -126,20 +127,24 @@ class _MemeCard extends StatelessWidget {
                       itemBuilder: (BuildContext ctx) {
                         HomeController homeCtrl = Get.find<HomeController>();
                         List<Widget> list = List.empty(growable: true);
-                        list.addIf(
-                            homeCtrl.appWechat.value,
-                            IconButton(
-                              onPressed: () {},
-                              icon: Image.memory(homeCtrl.appIcons["wechat"]),
-                            ));
-                        list.addIf(
-                            homeCtrl.appTim.value,
-                            IconButton(
+
+                        void Function(bool, String) buildItem =
+                            (bool isAdd, String app) {
+                          if (isAdd) {
+                            var item = IconButton(
                               onPressed: () {
-                                homeCtrl.sendToTim(model);
+                                homeCtrl.shareMemeTo(model, app);
                               },
-                              icon: Image.memory(homeCtrl.appIcons["tim"]),
-                            ));
+                              icon: Image.memory(homeCtrl.appIcons[app]),
+                            );
+                            list.add(item);
+                          }
+                        };
+                        buildItem(homeCtrl.appWechat.value, "wechat");
+
+                        buildItem(homeCtrl.appQQ.value, "qq");
+
+                        buildItem(homeCtrl.appTim.value, "tim");
                         return list
                             .map((e) => PopupMenuItem(child: e))
                             .toList(growable: false);
@@ -156,124 +161,3 @@ class _MemeCard extends StatelessWidget {
   }
 }
 
-class _SearchBarDelegate extends SearchDelegate<String> {
-  late List<String> tags;
-  late List<String> title;
-  MemeData db = Get.find<MemeData>();
-  final Widget Function(List<MemeModel>) _waterfallBuilder;
-
-  _SearchBarDelegate(this._waterfallBuilder) {
-    var items = db.data.cast<MemeModel>();
-    tags = items.expand((element) => element.tags).toList(growable: false);
-    title = items.map((e) => e.name).toList(growable: false);
-    title.sort();
-    tags.sort();
-  }
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return <Widget>[IconButton(onPressed: () {}, icon: Icon(Icons.search))];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: AnimatedIcon(
-          icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
-      onPressed: () {
-        if (query.isEmpty) {
-          close(context, "");
-        } else {
-          query = "";
-          showSuggestions(context);
-        }
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    var inputs = query
-        .split(" ")
-        .map((e) => e.trim())
-        .where((element) => element.isNotEmpty)
-        .toList(growable: false);
-    List<MemeModel> result = List.empty(growable: true);
-    ctrl:
-    for (int i = 0; i < db.data.length; i++) {
-      MemeModel e = db.data[i];
-      for (int j = 0; j < inputs.length; j++) {
-        if (!e.tags.contains(inputs[j]) &&
-            !e.name.toLowerCase().contains(inputs[j].toLowerCase())) {
-          continue ctrl;
-        }
-      }
-      result.add(db.data[i]);
-    }
-    return _waterfallBuilder(result);
-  }
-
-  List<String> _analyseQuery(String text) {
-    if (text.isEmpty) {
-      var all = this.tags + this.title;
-      all.sort();
-      return all;
-    }
-    var allInput = text
-        .split(" ")
-        .map((e) => e.trim())
-        .where((element) => element.isNotEmpty);
-    var input = allInput.last;
-    var properTag =
-        this.tags.where((element) => element.startsWith(input)).toList();
-    var properName =
-        this.title.where((element) => element.startsWith(input)).toList();
-    var result = properTag + properName;
-
-    if (result.contains(input)) {
-      result = _analyseQuery("");
-    }
-
-    allInput.forEach((element) {
-      if (result.contains(element)) {
-        result.remove(element);
-      }
-    });
-
-    return result;
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    Widget Function(Widget) wrap = (Widget w) => Padding(
-          padding: EdgeInsets.all(10),
-          child: w,
-        );
-    return wrap(Wrap(
-      spacing: 8,
-      children: _analyseQuery(query)
-          .map((e) => GestureDetector(
-                child: Chip(
-                  label: Text(e),
-                  deleteIcon: Icon(Icons.arrow_drop_up),
-                  onDeleted: () {},
-                ),
-                onTap: () {
-                  var allInput = query
-                      .split(" ")
-                      .map((e) => e.trim())
-                      .where((element) => e.isNotEmpty)
-                      .toList();
-                  if (e.startsWith(allInput.last)) {
-                    allInput[allInput.length - 1] = e;
-                  } else {
-                    allInput.add(e);
-                  }
-                  query =
-                      allInput.reduce((value, element) => "$value $element");
-                },
-              ))
-          .toList(growable: false),
-    ));
-  }
-}
